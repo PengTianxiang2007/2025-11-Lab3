5bacc30489c0318d9814e12242b8361e0b3fae44
Peng Tianxiang <932328811@qq.com>
2025-11-16 20:41:47 +0800

    带注释

--- 3A_handin.c
+++ 3A_handin.c
@@ -18,20 +18,20 @@ int main(){
 
     gets(inText);    // Read input line,and store it in the inText array,which is a character array of size 256.
 
-    if (strlen(inText) == 0) {
-        puts(outMorse); 
+    if (strlen(inText) == 0) {   //the output type of strlen is size_t type 
+        puts(outMorse);    //strlen: calculate the length(number of characters) of the input string
         return 0;      //if you just press enter without typing anything, output empty line
     }
 
     int is_only_spaces = 1; 
-    for (int i = 0; inText[i] != '\0'; i++) {
+    for (int i = 0; inText[i] != '\0'; i++) { //check each character until the null terminator is reached
         if (!isspace(inText[i])) { 
             is_only_spaces = 0; 
             break;             //check all characters to see if they are all spaces
-        }
+        }       //as long as one non-space character is found,set the flag to 0 and break the loop
     }
 
-    if (is_only_spaces) {
+    if (is_only_spaces) {  //if input is only spaces,is_only_spaces remains 1 and we output empty line
         puts(outMorse); 
         return 0;       //if input is only spaces, output empty line
     }
@@ -64,13 +64,15 @@ int main(){
         if (c >= 'A' && c <= 'Z') {  
             int index = c - 'A';      //char type are turned into int type for calculation
             strcat(outMorse, MORSE_AZ[index]);   //append Morse code for the letter
-
-            int j = i + 1;
-            while (inText[j] && (inText[j]==','||inText[j]=='.'||inText[j]=='!'||inText[j]=='?'||
+                    //to check if we need to add a space after the Morse code
+            int j = i + 1;    //if there is punctuation between letters/numbers,we need to add a space
+            while (inText[j] != '\0' && (inText[j]==','||inText[j]=='.'||inText[j]=='!'||inText[j]=='?'||
                                  inText[j]==';'||inText[j]==':'||inText[j]=='-'||inText[j]=='"'||inText[j]=='\''))
-                j++;
-            if (inText[j] != '\0' && !isspace(inText[j])) 
-                strcat(outMorse, " ");   //add space if next character is not punctuation or space
+                j++;   //if the next character(s) are punctuation,skip them
+                       //finish the loop when a non-punctuation character or null terminator is found
+                       //if j is at a number,letter,out the loop
+            if (inText[j] != '\0' && !isspace(inText[j])) //unless two special cases,add space
+                strcat(outMorse, " ");   
                             
 
             is_encoded = 1;      //set flag to indicate encoding has occurred
@@ -80,7 +82,7 @@ int main(){
             strcat(outMorse, MORSE_09[index]);
 
             int j = i + 1;
-            while (inText[j] && (inText[j]==','||inText[j]=='.'||inText[j]=='!'||inText[j]=='?'||
+            while (inText[j] != '\0' && (inText[j]==','||inText[j]=='.'||inText[j]=='!'||inText[j]=='?'||
                                  inText[j]==';'||inText[j]==':'||inText[j]=='-'||inText[j]=='"'||inText[j]=='\'')) j++;
             if (inText[j] != '\0' && !isspace(inText[j])) 
                 strcat(outMorse, " ");
@@ -89,7 +91,7 @@ int main(){
 
         } else if (c == ';' || c == ':' || c == '-' || c == '!' || c == '?' || c == '.' || c == '\''|| c == ','|| c == '"') {
             continue;  //skip punctuation characters
-        } else if (isspace(c)) {
+        } else if (isspace(c)) {//to avoid the continus "/" by examineing the previous character
             if (i > 0) {                        //make sure i-1 is valid
                 if (!isspace(inText[i-1])) {    //check whether the previous character was not a space
                     if (is_encoded == 1) {      //make sure the "/" will not be added at the beginning
@@ -100,7 +102,7 @@ int main(){
         }
     }
 
-    int final_len = strlen(outMorse);
+    int final_len = strlen(outMorse);//to locate the last character because if it only appear once if it's "/"
     if (final_len > 0) {
         char last_char = outMorse[final_len - 1];//locate the last character and check if it is space or "/"
         if (last_char == ' ' || last_char == '/') {
--- 3B.c
+++ 3B.c
@@ -13,28 +13,27 @@ int main(){
     int n;
     scanf("%d",&n);
 
-    Record records[1009];
+    Record records[1000];  //define an array of struct
 
     for (int i = 0; i < n; i++) {
         scanf("%s %d %d:%d", records[i].name, &records[i].score, &records[i].hour, &records[i].minute);
         records[i].time = records[i].hour*60 + records[i].minute;
-    }
+    }                      //input the data and calculate the differece in minutes
 
-    for (int i = 0 ; i < n - 1 ; i++){
+    for (int i = 0 ; i < n - 1 ; i++){  //n-1 is enough because the last one will be in the right position automatically
         Record temp;
-        for (int j = 0 ; j < n - 1 - i ; j++){
-            if (records[j].score < records[j+1].score){
-                temp = records[j];
+        for (int j = 0 ; j < n - 1 - i ; j++){ //n-1-i beacuse some large score have already been in the right position
+            if (records[j].score < records[j+1].score){  
+                temp = records[j];  //every out loop will define the position of the relatively highest score
                 records[j] = records[j+1];
                 records [j+1] = temp;
             }
-            
         }
     }
 
     for (int i = 0 ; i < n - 1 ; i++ ){
         Record temp;
-        for (int j = 0 ; j < n - 1 - i ; j++){
+        for (int j = 0 ; j < n - 1 - i ; j++){  //bubble the sort based on time if score is the same
             if (records[j].score == records[j+1].score && records[j].time > records[j+1].time){
                 temp = records[j];
                 records[j] = records[j+1];
@@ -48,7 +47,7 @@ int main(){
         for (int j = 0 ; j < n - 1 - i ; j++){
             if (records[j].score == records[j+1].score && records[j].time == records[j+1].time && 
             strcmp(records[j].name , records[j+1].name)>0){
-                temp = records[j];
+                temp = records[j];          //bubble the sort based on name if score and time are the same
                 records[j] = records[j+1];
                 records [j+1] = temp;
             }
@@ -56,7 +55,7 @@ int main(){
     }
  
     for (int i = 0 ; i < n ; i++ ){
-        printf("%s\n",records[i].name);
+        printf("%s\n",records[i].name);  //output the names in order
     }
 
     
